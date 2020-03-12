10b1bdce641b07e605bf98d0c9f5d919089efb02
SilurianYang <1606726660@qq.com>
2020-03-12 13:13:21 +0800

    modify dev and build config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,34 +1,4 @@
-const path = require("path");
-
-const version=process.env.npm_package_version;
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: `uni-read-pages@${version}.js`,
-    path: path.resolve(__dirname, '../','dist'),
-    library:'TransformPages',
-    libraryTarget:'commonjs2'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/, 
-        use: {
-           loader: 'babel-loader', 
-           options: {  
-                presets: [
-                    ['@babel/preset-env', {
-                        targets: {
-                            browsers: ['> 1%', 'last 2 version']
-                        } 
-                    }]
-                    
-                ] 
-           }
-        },
-        exclude: '/node_module/'
-    }
-    ]
-  }
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,15 +1,27 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = merge(common, {
-  mode:'development',
-  output:{
+  mode: 'development',
+  output: {
     filename: `uni-read-pages-bundle.js`,
-    path: path.resolve(__dirname, '../','examples/common'),
+    path: path.resolve(__dirname, '../', 'examples/common'),
   },
   devServer: {
     contentBase: "./examples/common"
   },
+  plugins: [
+    new CopyPlugin([{
+      context: './src/',
+      force: true,
+      from: './index.js',
+      to: `uni-read-pages-bundle.js`,
+      globOptions: {
+        copyUnmodified: true
+      }
+    }]),
+  ]
 });
